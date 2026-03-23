f6678577e5597024dc449ad42d5cd8974bfad34a
Lord <mtt.mallee@gmail.com>
2026-03-23 15:27:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ if not st.session_state.auth:
     else:
         st.markdown("<h1 style='text-align:center;'>🛡️</h1>", unsafe_allow_html=True)
 
-    # CORRECTION ICI : Suppression de unsafe_allow_index
+    # REPARATION : Argument invalide supprimé
     st.markdown('<div class="hanna-main-title">HANNA</div>', unsafe_allow_html=True)
     st.markdown('<div class="hanna-sub-title">Hybrid Adaptive Navigator & Network Assistant</div>', unsafe_allow_html=True)
     
